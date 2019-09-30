178c76f940c8e705f08528b4dd8d2248ab56a4db
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-30 17:29:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file,emoticon)
   ymal_hash = load_library(file)
   convert_emoticon = ymal_hash[:get_emoticon][emoticon]
   if convert_emoticon == nil
-    convert_emoticon =  "Our apologies, the emoticon is not available."
+    convert_emoticon =  "Sorry, that emoticon was nott found"
   end
   return convert_emoticon
 end
